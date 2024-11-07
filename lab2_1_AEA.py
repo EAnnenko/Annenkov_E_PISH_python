de85ca3869babbfee959649b79f8b9484ef70524
EAnnenkov <e.annenkov.nck@gmail.com>
2024-11-07 23:34:19 +0300

    Add files via upload

---
+++ lab2_1_AEA.py
@@ -0,0 +1,12 @@
+#Лабораторная №2 задача 1
+x = int(input("Введите х: "))
+if -2 <= x < 2:
+    a = x ** 2
+    print (a)
+else:
+    if x >=2:
+        b = x ** 2 + 4 * x + 5
+        print (b)
+    else:
+        if x < -2:
+            print (4)
