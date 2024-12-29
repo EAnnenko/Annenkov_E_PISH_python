ccb925d071f1a1e2b8b5da2da72e64e4c516e099
EAnnenkov <e.annenkov.nck@gmail.com>
2024-12-29 11:16:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from my_packege import module1
+from my_packege import module2
+from my_packege import module3
+from my_packege import module4
+print (module1)
+print (module2)
+print (module3)
+print (module4)
---
+++ module2.py
@@ -0,0 +1,11 @@
+#Лабораторная №2 задача 2
+a = int(input(""" Лабораторная №2 задача 2
+              Введите число а: """)) 
+b = int(input(" Введите число b: "))
+def function(a,b): 
+    while(b): 
+        a, a = b, a % b 
+        return a  
+c = function(a, b)
+print("Наибольший общий делитель") 
+print(c)
