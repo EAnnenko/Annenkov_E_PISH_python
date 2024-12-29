c4ccdd5d0bee42222e91cbf31064acdfcca6f2f3
EAnnenkov <e.annenkov.nck@gmail.com>
2024-12-29 11:13:44 +0300

    Add files via upload

---
+++ lab7_2.py
@@ -0,0 +1,3 @@
+numbers = [10, 15, 20, 25, 30]
+evens = list(filter(lambda x: x % 2 == 0, numbers))
+print(evens)
---
+++ lab7_3.py
@@ -0,0 +1,3 @@
+words = ["apple", "banana", "pear", "grape", "kiwi"]
+sorted_words = sorted(words, key=lambda x: len(x))
+print(sorted_words)
